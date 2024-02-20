dc0d0f2ef728066a70cf5ff19f246c7ec9115863
Your Name <remyguillery@yahoo.fr>
2024-02-20 12:57:53 +0100

    Mise en place du formulaire, des relations entites, de la securite, d'envoi d'email en cas de mot de passe oublie et enfin d'un service qui envoi un mail une fois un nouveau compte cree

---
+++ src/Notification/Sender.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Notification;
+
+
+
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class Sender
+{
+    public function __construct(protected MailerInterface $mailer)
+    {
+    }
+
+    public function sendNewUserNotificationToAdmin(UserInterface $user)
+    {
+        //file_put_contents('debug.txt', $user->getEmail());
+
+        $message = new Email();
+        $message->from('accounts@series.com')
+            ->to('admin@series.com')
+            ->subject('new account created on series.com')
+            ->html('<h1>New account</h1> email: ' . $user->getEmail());
+
+        $this->mailer->send();
+    }
+}
